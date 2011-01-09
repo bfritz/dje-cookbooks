6fe45eb7650277dd28b12fdc60ad7c250e1f5216
Darrin Eden <darrin.eden@gmail.com>
2011-01-08 21:56:24 -0800

    unicornapp deleted 'cause it wasn't a good idea

--- unicornapp/attributes/unicorn.rb
+++
@@ -1,2 +0,0 @@
-default[:unicorn][:environment] = "production"
-default[:unicorn][:copy_on_write_friendly] = "false"
--- unicornapp/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer        "Heavy Water Software Inc."
-maintainer_email  "darrin@heavywater.ca"
-license           "Apache 2.0"
-description       "Installs and configures unicorn"
-version           "0.0.1"
-
-depends "bluepill"
-
-supports "ubuntu"
