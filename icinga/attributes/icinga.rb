cc2163908bbc5136a9150ec722a660e6aacb15e5
Darrin Eden <darrin.eden@gmail.com>
2011-03-09 18:10:10 -0800

    beginning work on an icinga cookbook

---
+++ icinga/attributes/icinga.rb
@@ -0,0 +1,2 @@
+default[:icinga][:version] = "1.3.0"
+default[:icinga][:checksum] = "eec3b6ff"
---
+++ icinga/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Heavy Water Software Inc."
+maintainer_email "darrin@heavywater.ca"
+license          "Apache 2.0"
+description      "Installs/Configures icinga"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+supports "ubuntu"
+supports "centos"
