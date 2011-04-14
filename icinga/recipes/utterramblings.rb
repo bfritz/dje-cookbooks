bec48fb3dc640f04992a36c18a814e119e073431
Darrin Eden <darrin.eden@gmail.com>
2011-04-14 15:52:22 -0700

    cleanup more redhat vomit

--- icinga/recipes/utterramblings.rb
+++
@@ -1,8 +0,0 @@
-execute "install key" do
-  command "rpm --import http://www.jasonlitka.com/media/RPM-GPG-KEY-jlitka"
-  action :nothing
-end
-
-template "/etc/yum.repos.d/utterramblings.repo" do
-  notifies :run, "execute[install key]", :immediately
-end
