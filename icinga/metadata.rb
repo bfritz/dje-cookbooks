b10c0090123d0ba8d71259db52fa98582cee8ae2
Darrin Eden <darrin.eden@gmail.com>
2011-04-14 15:51:53 -0700

    depends on php cookbook

--- icinga/metadata.rb
+++ icinga/metadata.rb
@@ -5,6 +5,7 @@ description      "Installs/Configures icinga"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
+depends "php"
 depends "apache2"
 depends "mysql"
 
