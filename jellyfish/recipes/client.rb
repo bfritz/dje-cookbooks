2d0bd346bafd4963ccd437c06154683943e3c1e1
Darrin Eden <darrin.eden@gmail.com>
2011-05-16 14:51:16 -0700

    jellyfish client init

--- jellyfish/recipes/client.rb
+++ jellyfish/recipes/client.rb
@@ -14,3 +14,14 @@ template "/etc/jellyfish.yml" do
              :endpoint => node[:jellyfish][:endpoint],
              :command => node[:jellyfish][:command] )
 end
+
+template "/etc/init/jellyfish-client.conf" do
+  source "jellyfish-upstart.erb"
+  notifies :restart, "service[jellyfish-client]"
+end
+
+service "jellyfish-client" do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
